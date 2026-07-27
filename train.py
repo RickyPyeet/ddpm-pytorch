79dd262e8fdac1f283429d3a7b6d97a2230f2b7c
Riccardo <ricca.pit@hotmail.it>
2026-07-27 15:18:16 +0200

    Update train to take logger configuration for experimentation

--- train.py
+++ train.py
@@ -1,52 +1,60 @@
 import torch
-from torch import nn
-from src.ddpm.utils.seed import set_seed
-from src.ddpm.utils.config import load_config
+
 from src.ddpm.data.cifar10 import get_cifar10_dataloader
 from src.ddpm.models.conditioned_unet import ClassConditionedUNet
 from src.ddpm.training.trainer import trainer
+from src.ddpm.training.wandb import init_wandb
+from src.ddpm.utils.config import load_config
+from src.ddpm.utils.seed import set_seed
 
-def main():
 
-  config = load_config('configs/cifar10.yaml')
+def main() -> None:
+    config = load_config("configs/cifar10.yaml")
 
-  set_seed(config['seed'])
-  
-  device = config['device']
-  if device == 'cuda' and not torch.cuda.is_available():
-    device = 'cpu'
+    training_config = config["training"]
+    dataloader_config = config["dataloader"]
+    model_config = config["model"]
+    paths_config = config["paths"]
+    sampling_config = config["sampling"]
 
-  train_dataloader = get_cifar10_dataloader(data_dir = config['dataset']['root'],
-                                            batch_size = config['dataset']['batch_size'],
-                                            num_workers = config['dataset']['num_workers'])
-  
-  model = ClassConditionedUNet(num_classes = config['model']['num_classes'],
-                               input_dim = config['model']['input_dim'],
-                               channels = config['model']['channels'],
-                               groupnorm_groups = config['model']['groupnorm_groups'],
-                               dimension_multiplier = tuple(config['model']['dimension_multiplier']))
+    set_seed(training_config["seed"])
 
-  trainer(model=model,
-          train_dataloader=train_dataloader,
-          epochs=config['training']['epochs'],
-          device=device,
-          pred_type=config['diffusion']['prediction_type'],
-          lr=config['training']['learning_rate'],
-          class_free_dropout=config['cfg']['dropout'],
-          guidance_scale=config['cfg']['guidance_scale'],
-          eta=config['sampling']['eta'],
-          ema_decay=config['training']['ema_decay'],
-          timesteps=config['diffusion']['timesteps'],
-          schedule_type=config['diffusion']['schedule'],
-          optim=config['training']['optimizer'],
-          save_dir=config['training']['save_dir'],
-          save_every=config['training']['save_every'],
-          resume_from=config['training']['resume_from'],
-          sample_every=config['sampling']['sample_every'],
-          sample_labels=torch.tensor(config['sampling']['labels']),
-          sampler=config['sampling']['sampler'],
-          sample_timesteps=config['sampling']['sampling_timesteps'],
-          use_compile=config['training']['use_compile'])
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-if __name__ == '__main__':
-  main()
+    train_dataloader = get_cifar10_dataloader(
+        data_dir=paths_config["dataset_path"],
+        batch_size=dataloader_config["batch_size"],
+        num_workers=dataloader_config["num_workers"]
+        )
+
+    model = ClassConditionedUNet(
+        num_classes=model_config["num_classes"],
+        input_dim=model_config["input_dim"],
+        channels=model_config["in_channels"],
+        groupnorm_groups=model_config["groupnorm_groups"],
+        dimension_multiplier=tuple(model_config["dimension_multiplier"]))
+
+    experiment_config = {
+        **training_config,
+        **dataloader_config,
+        **model_config}
+
+    wandb_run = init_wandb(
+        wandb_config=config["wandb"],
+        experiment_config=experiment_config)
+
+    trainer(
+        model=model,
+        train_dataloader=train_dataloader,
+        device=device,
+        save_dir=paths_config["save_path"],
+        resume_from=paths_config["resume_from"],
+        cifar_val_root=paths_config["cifar_val_path"],
+        wandb_run=wandb_run,
+        sample_labels=torch.tensor(sampling_config["sample_labels"], dtype=torch.long, device=device),
+        **training_config
+    )
+
+
+if __name__ == "__main__":
+    main()
